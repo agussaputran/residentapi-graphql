8021328de9efc98598cd53783727a5b67992dc62
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-13 21:22:46 +0700

    feat(fieldtypes): add district type

---
+++ fieldtypes/districtType.go
@@ -0,0 +1,20 @@
+package fieldtypes
+
+import "github.com/graphql-go/graphql"
+
+// DistrictType type
+func DistrictType() *graphql.Object {
+	return graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "District",
+			Fields: graphql.Fields{
+				"id": &graphql.Field{
+					Type: graphql.Int,
+				},
+				"name": &graphql.Field{
+					Type: graphql.String,
+				},
+			},
+		},
+	)
+}
