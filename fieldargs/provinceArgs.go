1201fbb1c54e78bb1e5c68f477c45c77ec11a157
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-13 22:12:48 +0700

    feat(fieldargs): add province field args

---
+++ fieldargs/provinceArgs.go
@@ -0,0 +1,33 @@
+package fieldargs
+
+import "github.com/graphql-go/graphql"
+
+// CreateProvinceArgs for mutation args
+func CreateProvinceArgs() graphql.FieldConfigArgument {
+	return graphql.FieldConfigArgument{
+		"name": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+	}
+}
+
+// UpdateProvinceArgs for mutation args
+func UpdateProvinceArgs() graphql.FieldConfigArgument {
+	return graphql.FieldConfigArgument{
+		"id": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.Int),
+		},
+		"name": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+	}
+}
+
+// DeleteProvinceArgs for mutation args
+func DeleteProvinceArgs() graphql.FieldConfigArgument {
+	return graphql.FieldConfigArgument{
+		"id": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.Int),
+		},
+	}
+}
