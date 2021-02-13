f8e450af2ba40704ab797ac1a9826abd885b570a
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-13 22:03:42 +0700

    feat(api): add create province mutation

---
+++ api/mutation.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"resident-graphql/fieldtypes"
+	"resident-graphql/resolvers"
+
+	"github.com/graphql-go/graphql"
+)
+
+func MutationType() *graphql.Object {
+	return graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "Mutation",
+			Fields: graphql.Fields{
+				"create_province": &graphql.Field{
+					Type:        fieldtypes.ProvinceType(),
+					Description: "Create new province",
+					Args:        typesargs.CreateProvinceArgs(),
+					Resolve:     resolvers.CreateProvinceResolver,
+				},
+			},
+		},
+	)
+}
