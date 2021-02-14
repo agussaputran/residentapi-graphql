43db1733247cb55628d77744d871fa63c664c3ff
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-14 22:59:13 +0700

    feat(resolvers): create createDistrictresolver

--- resolvers/districtResolve.go
+++ resolvers/districtResolve.go
@@ -1,12 +1,30 @@
 package resolvers
 
 import (
+	"math/rand"
 	"resident-graphql/connection"
 	"resident-graphql/models/responses"
 	"resident-graphql/models/tables"
+	"time"
 
 	"github.com/graphql-go/graphql"
 )
+
+// CreateDistrictResolver func
+func CreateDistrictResolver(p graphql.ResolveParams) (interface{}, error) {
+	db := *connection.GetConnection()
+	rand.Seed(time.Now().UnixNano())
+
+	var district tables.Districts
+
+	district.ID = uint(rand.Intn(100000))
+	district.DistrictName = p.Args["district_name"].(string)
+	district.ProvinceID = p.Args["province_id"].(uint)
+
+	db.Create(&district)
+
+	return district, nil
+}
 
 // ReadDistrictResolver func
 func ReadDistrictResolver(p graphql.ResolveParams) (interface{}, error) {
