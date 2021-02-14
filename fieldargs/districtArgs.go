e982cabe7f002f51e5a13a5332a472002f243d55
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-14 22:49:53 +0700

    feat(fieldargs): create district args

---
+++ fieldargs/districtArgs.go
@@ -0,0 +1,33 @@
+package fieldargs
+
+import "github.com/graphql-go/graphql"
+
+// CreateDistrictArgs for mutation args
+func CreateDistrictArgs() graphql.FieldConfigArgument {
+	return graphql.FieldConfigArgument{
+		"district_name": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+	}
+}
+
+// UpdateDistrictArgs for mutation args
+func UpdateDistrictArgs() graphql.FieldConfigArgument {
+	return graphql.FieldConfigArgument{
+		"id": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.Int),
+		},
+		"district_name": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+	}
+}
+
+// DeleteDistrictArgs for mutation args
+func DeleteDistrictArgs() graphql.FieldConfigArgument {
+	return graphql.FieldConfigArgument{
+		"id": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.Int),
+		},
+	}
+}
