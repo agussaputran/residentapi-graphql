971eece4f30db29e4ca2955b103387a4012583bc
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-15 10:21:29 +0700

    feat(fieldargs): create person field args

---
+++ fieldargs/personArgs.go
@@ -0,0 +1,57 @@
+package fieldargs
+
+import "github.com/graphql-go/graphql"
+
+// CreatePersonArgs for mutation args
+func CreatePersonArgs() graphql.FieldConfigArgument {
+	return graphql.FieldConfigArgument{
+		"nip": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"full_name": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"first_name": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"last_name": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"birth_date": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"birth_place": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"gender": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"zone_location": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+		"sub_district_id": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.Int),
+		},
+	}
+}
+
+// UpdatePersonArgs for mutation args
+func UpdatePersonArgs() graphql.FieldConfigArgument {
+	return graphql.FieldConfigArgument{
+		"id": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.Int),
+		},
+		"full_name": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.String),
+		},
+	}
+}
+
+// DeletePersonArgs for mutation args
+func DeletePersonArgs() graphql.FieldConfigArgument {
+	return graphql.FieldConfigArgument{
+		"id": &graphql.ArgumentConfig{
+			Type: graphql.NewNonNull(graphql.Int),
+		},
+	}
+}
