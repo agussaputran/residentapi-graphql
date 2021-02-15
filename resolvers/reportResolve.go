a88c93e64af77279e3489bd7c108827c805ec09c
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-15 14:30:04 +0700

    feat(resolvers): create ReadReportPersonOfficeByGenderResolver

--- resolvers/reportResolve.go
+++ resolvers/reportResolve.go
@@ -19,3 +19,17 @@ func ReadReportGenderResolver(p graphql.ResolveParams) (interface{}, error) {
 		Select("gender, count(*) as total").Group("gender").Scan(&response)
 	return response, nil
 }
+
+// ReadReportPersonOfficeByGenderResolver func
+func ReadReportPersonOfficeByGenderResolver(p graphql.ResolveParams) (interface{}, error) {
+	db := *connection.GetConnection()
+	var (
+		person   []tables.Persons
+		response []responses.ReportGenderResponse
+	)
+	db.Model(&person).
+		Select("gender, count(*) as total").
+		Joins("join office_person_locations on persons.id = office_person_locations.person_id").
+		Group("gender").Scan(&response)
+	return response, nil
+}
