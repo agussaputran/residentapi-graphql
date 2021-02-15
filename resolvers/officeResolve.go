fdd1dd761fda39095b95f2959cee784ce19f1ef0
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-15 13:30:46 +0700

    feat(resolvers): create CreateOfficeResolver

--- resolvers/officeResolve.go
+++ resolvers/officeResolve.go
@@ -1,12 +1,30 @@
 package resolvers
 
 import (
+	"math/rand"
 	"resident-graphql/connection"
 	"resident-graphql/models/responses"
 	"resident-graphql/models/tables"
+	"time"
 
 	"github.com/graphql-go/graphql"
 )
+
+// CreateOfficeResolver func
+func CreateOfficeResolver(p graphql.ResolveParams) (interface{}, error) {
+	db := *connection.GetConnection()
+	rand.Seed(time.Now().UnixNano())
+
+	var office tables.Offices
+
+	office.ID = uint(rand.Intn(100000))
+	office.OfficeName = p.Args["office_name"].(string)
+	office.SubDistrictID = uint(p.Args["sub_district_id"].(int))
+
+	db.Create(&office)
+
+	return office, nil
+}
 
 // ReadOfficeResolver func
 func ReadOfficeResolver(p graphql.ResolveParams) (interface{}, error) {
