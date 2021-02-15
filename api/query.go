48988418391a9540a120cd7306fd57e42cc98cd8
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-15 08:43:58 +0700

    refactoring(fieldtypes); change subDistricttype func name

--- api/query.go
+++ api/query.go
@@ -23,6 +23,11 @@ func QueryType() *graphql.Object {
 					Description: "Get District List",
 					Resolve:     resolvers.ReadDistrictResolver,
 				},
+				"read_sub_district": &graphql.Field{
+					Type:        graphql.NewList(fieldtypes.Subs()),
+					Description: "Get District List",
+					Resolve:     resolvers.ReadDistrictResolver,
+				},
 			},
 		},
 	)
