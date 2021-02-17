dc154da360682dea31659d78f016588d50e8178c
Agus Ntoi <ntoiagussaputra@gmail.com>
2021-02-17 21:47:46 +0700

    delete all env

--- helper/envHelper.go
+++ helper/envHelper.go
@@ -1,25 +1,7 @@
 package helper
 
-import (
-	"os"
-)
-
 // Token global
 var Token string
 
 // ReqBody global
 var ReqBody string
-
-// SetEnvVar func
-func SetEnvVar() {
-	os.Setenv("DB_HOST", "localhost")
-	os.Setenv("DB_USER", "postgres")
-	os.Setenv("DB_PASSWORD", "r3m3mb3r34")
-	os.Setenv("DB_NAME", "resident-graphql")
-	os.Setenv("DB_PORT", "5432")
-	os.Setenv("DB_SSL", "disable")
-	os.Setenv("DB_TIMEZONE", "Asia/Shanghai")
-	os.Setenv("JWT_SECRET", "kodebom")
-	os.Setenv("RAJAONGKIR_APIKEY", "366cf34aabf54556c13fd48951aadf3a")
-	os.Setenv("DSN", "https://a01e543958d44f51b3860e928ca45980@o515018.ingest.sentry.io/5619088")
-}
